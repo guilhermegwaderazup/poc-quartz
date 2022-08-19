db961fc427123870b07f33b4617995a81e1b8c38
Guilherme Gwadera <guilherme.gwadera@zup.com.br>
2022-08-19 15:58:47 -0300

    poc schema separado para o quartz no DB

--- src/main/java/com/example/pocbatch/PocBatchApplication.java
+++ src/main/java/com/example/pocbatch/PocBatchApplication.java
@@ -1,8 +1,15 @@
 package com.example.pocbatch;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.autoconfigure.quartz.SchedulerFactoryBeanCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.sql.DataSource;
 
 @EnableScheduling
 @SpringBootApplication
@@ -10,6 +17,28 @@ public class PocBatchApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PocBatchApplication.class, args);
+    }
+
+    @Bean
+    public SchedulerFactoryBeanCustomizer schedulerFactoryBeanCustomizer(final DataSourceProperties dataSourceProperties) {
+        return schedulerFactoryBean -> {
+            DataSource dataSource = dataSourceProperties.initializeDataSourceBuilder()
+                    .url(dataSourceProperties.getUrl() + "?currentSchema=scheduler")
+                    .build();
+            schedulerFactoryBean.setDataSource(dataSource);
+        };
+    }
+
+    @Bean
+    public FlywayMigrationStrategy flywayMigrationStrategy() {
+        return flyway -> {
+            Flyway.configure()
+                    .locations("db/migration/quartz")
+                    .dataSource(flyway.getConfiguration().getDataSource())
+                    .schemas("scheduler")
+                    .load()
+                    .migrate();
+        };
     }
 
 
